a9655968208241fd85eb1f1fd0e1172c3d3fa935
musa jahun <msjahun@live.com>
2022-09-20 06:57:53 +0100

    Worked on the design of the onboarding page, looks good on the emulator

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Marketyeri',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: const Authentication(),
     );
